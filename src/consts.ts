dd26208b0941a1bfe6daaf416d0c20f84fe3b563
DaniRiv01 <drivesivila@gmail.com>
2026-03-12 13:42:18 +0100

    Primer commit del portfolio

---
+++ src/consts.ts
@@ -0,0 +1,18 @@
+export const SITE_INFO = {
+  name: 'Daniel Rives Sivila',
+  role: 'Desarrollador Web',
+  description: 'Portfolio de Daniel Rives Sivila - Desarrollador Web (Por que diseñador poco).',
+};
+
+export const SKILLS = [
+  "HTML5",
+  "CSS3",
+  "JavaScript",
+  "TypeScript",
+  "Astro",
+  "Tailwind CSS",
+  "React",
+  "Git",
+  "GitHub",
+  "Figma",
+];
---
+++ src/data/proyectos.ts
@@ -0,0 +1,25 @@
+// src/data/proyectos.ts
+
+export const proyectos = [
+  {
+    titulo: "Launch Page para Hollow Knight: Silksong",
+    descripcion: "Página dedicada a la presentación del aquel entonces nuevo juego de la historia de Hollow Knight.",
+    tags: ["Proyectos", "Juegos", "Astro"],
+    imagen: "/proyectos/Silksong.avif",
+    enlace: "#",
+  },
+  {
+    titulo: "Página de información de Valorant",
+    descripcion: "Una página con la que se buscaba la simplicidad a la hora de hallar información acerca de Agentes, mapas y armas.",
+    tags: ["Proyectos", "Juegos", "Astro"],
+    imagen: "/proyectos/Valorant.jpg",
+    enlace: "#",
+  },
+  {
+    titulo: "Personajes Dragon Ball",
+    descripcion: "Un sitio web donde puedes encontrar varios personajes de la mítica serie animada Dragon Ball.",
+    tags: ["Proyectos", "Anime", "Astro"],
+    imagen: "/proyectos/D-Ball.webp",
+    enlace: "#",
+  },
+];
